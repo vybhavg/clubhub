babc27cd9df6218ea3c651adde1149ace6de7027
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-03 19:52:37 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,1 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+// Database connection settings
+$db_host = '127.0.0.1'; // Localhost for the same EC2 instance
+$db_username = 'root'; // Replace with your database username
+$db_password = 'Vybhav@123ABC!'; // Replace with your database password
+$db_name = 'mydatabase'; // Replace with your database name
+
+// Create a connection to the database
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
