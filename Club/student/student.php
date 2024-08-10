3a9bc64c2f048d3a1dd89d1886ec397de2446e85
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-10 16:10:45 +0530

    Update student.php

--- Club/student/student.php
+++ Club/student/student.php
@@ -1,4 +1,17 @@
 <?php
+$test_file = 'uploads/test.txt';
+$file = fopen($test_file, 'w');
+if ($file) {
+    fwrite($file, 'Test content');
+    fclose($file);
+    echo 'File written successfully';
+} else {
+    echo 'Unable to write file';
+}
+?>
+
+
+<?php
 // Include database connection
 require '/var/www/html/db_connect.php';
 
