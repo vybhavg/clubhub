c0e002ed7de676c6c511a239d57064965cc34df3
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-24 16:41:51 +0530

    Update members.php

--- Club/login/members/members.php
+++ Club/login/members/members.php
@@ -5,6 +5,7 @@ include('/var/www/html/db_connect.php'); // Include your database connection fil
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+//hi
 
 // Set default values for the first visit after login
 if (!isset($_SESSION['selected_branch'])) {
