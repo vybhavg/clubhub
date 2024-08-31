a5dae9bdf1b74ff09e2ebd786ee3860e87fe8fbf
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 16:23:02 +0530

    Update login.js

--- Club/login/login.js
+++ Club/login/login.js
@@ -10,18 +10,6 @@
     });
 
     // Toggle between login and registration forms
-    const container = document.getElementById('container');
-    const registerBtn = document.getElementById('register');
-    const loginBtn = document.getElementById('login');
 
-    if (registerBtn && loginBtn && container) {
-        registerBtn.addEventListener('click', () => {
-            container.classList.add("active");
-        });
-
-        loginBtn.addEventListener('click', () => {
-            container.classList.remove("active");
-        });
-    }
 
 })(jQuery);
