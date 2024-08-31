3f1435c4e5d1f1dc8eff1b4b092a4fd50bd10b72
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 18:21:44 +0530

    Update login.php

--- Club/login/login.php
+++ Club/login/login.php
@@ -112,7 +112,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login/Register</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
     <style>
         /* Add your CSS styles */
     </style>
