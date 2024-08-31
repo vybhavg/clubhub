0cc7cbcec6ceeb5d210bfd52b00f9e0a8d3a05e1
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 19:10:05 +0530

    Update login.js

--- Club/login/login.js
+++ Club/login/login.js
@@ -1,15 +1,9 @@
-(function($) {
-    "use strict";
-
-    // Show/Hide Password
-    $(document).on('click', '.btn-show-pass', function() {
-        const input = $(this).siblings('input'); // Use siblings if the button is not immediately next to the input
-        const isPasswordVisible = input.attr('type') === 'text';
-        input.attr('type', isPasswordVisible ? 'password' : 'text');
-        $(this).toggleClass('active', !isPasswordVisible);
+// Smooth scrolling for anchor links
+document.querySelectorAll('a.scrollto').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-
-    // Toggle between login and registration forms
-
-
-})(jQuery);
+});
