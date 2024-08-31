880398b975b7ae36cb7d9855001a02d9a801d2ee
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 15:05:39 +0530

    Update login.php

--- Club/login/login.php
+++ Club/login/login.php
@@ -96,7 +96,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="https://github.com/cdahaoui/Modern-Login-Page/blob/main/style.css">
+    <link rel="stylesheet" href="login.css">
     <title>Modern Login Page</title>
 </head>
 
@@ -157,7 +157,7 @@ $conn->close();
         </div>
     </div>
 
-    <script src="https://github.com/cdahaoui/Modern-Login-Page/blob/main/script.js"></script>
+    <script src="login.js"></script>
 </body>
 
 </html>
