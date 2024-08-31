20188e16bf1d7b8ec6d38cef01513171d8c70a4d
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 15:21:42 +0530

    Update login.js

--- Club/login/login.js
+++ Club/login/login.js
@@ -1,43 +1,4 @@
-(function($) {
-    "use strict";
 
-    // Input Validation and Interaction
-    $('.input100').on('blur', function() {
-        $(this).toggleClass('has-val', $(this).val().trim() !== "");
-    });
-
-    $('.validate-form').on('submit', function() {
-        let isValid = true;
-        $('.validate-input .input100').each(function() {
-            if (!validate(this)) {
-                showValidate(this);
-                isValid = false;
-            }
-        });
-        return isValid;
-    });
-
-    $('.validate-form .input100').on('focus', function() {
-        hideValidate(this);
-    });
-
-    function validate(input) {
-        const value = $(input).val().trim();
-        const type = $(input).attr('type');
-        const name = $(input).attr('name');
-        if (type === 'email' || name === 'email') {
-            return /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/.test(value);
-        }
-        return value !== '';
-    }
-
-    function showValidate(input) {
-        $(input).parent().addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        $(input).parent().removeClass('alert-validate');
-    }
 
     // Show/Hide Password
     $('.btn-show-pass').on('click', function() {
