3c5a63b053b8c13eaab3524bb5d895aeecb542c9
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 19:12:25 +0530

    Update login.js

--- Club/login/login.js
+++ Club/login/login.js
@@ -1,9 +1,1 @@
-// Smooth scrolling for anchor links
-document.querySelectorAll('a.scrollto').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+
