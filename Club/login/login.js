b9ccb6ae25709b25ae30caa1dfcebeac96c98a1e
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 15:18:54 +0530

    Update login.js

--- Club/login/login.js
+++ Club/login/login.js
@@ -47,17 +47,16 @@
         $(this).toggleClass('active', !isPasswordVisible);
     });
 
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
-
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
 
 })(jQuery);
