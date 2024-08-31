1044213c02c03d4a4d489dfd972df808cb60c689
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-08-31 14:33:35 +0530

    Update login.php

--- Club/login/login.php
+++ Club/login/login.php
@@ -96,7 +96,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
     <title>Modern Login Page</title>
 </head>
 
