2ea37c1aa063a053e28d4d09158f7f18b45f5cd4
vybhav <126269634+vybhavg@users.noreply.github.com>
2024-09-10 19:55:38 +0530

    Update track_location.php

--- attendance/track_location.php
+++ attendance/track_location.php
@@ -1,5 +1,8 @@
 <?php
 include('/var/www/html/db_connect.php'); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 header('Content-Type: application/json'); // Ensure the response is JSON
